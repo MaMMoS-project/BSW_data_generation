a15797ad85ac31b17ce337f75bb1422976243a7c
Sam Holt <samjrholt@gmail.com>
2026-02-09 15:21:37 +0100

    refactor reading

--- collect_data.py
+++ collect_data.py
@@ -1,18 +1,19 @@
 import pathlib as pl
+from collections import defaultdict
 import mammos_entity as me
-
 
 paths = sorted(pl.Path("data").glob("**/parameters.yaml"))
 
-collection = {"filepath": []}
+collection = defaultdict(list)
+filepath = []
 
 for file in paths:
-    collection_single = me.io.entities_from_file(file)    
-    for k, v in collection_single.items():        
-        if k not in collection:
-            collection[k] = v
-        else:
-            collection[k] = me.concat_flat(collection[k], v)
-    collection["filepath"].append(str(file))
-        
-me.io.entities_to_file("full.csv", **collection)
+    collection_single = me.io.entities_from_file(file)
+    for k, v in vars(collection_single).items():
+        collection[k].append(v)
+    filepath.append(str(file))
+
+for k, v in collection.items():
+    collection[k] = me.concat_flat(*v)
+
+me.io.entities_to_file("full.csv", **collection, filepath=filepath)
